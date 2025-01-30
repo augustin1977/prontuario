d472a069c472e94b36afe915725c60dc7c4f0aab
Eric_Augustin <ericaugustin@hotmail.com>
2025-01-30 16:30:07 -0300

    ajustando mensagens de erro

--- Usuario_django/views.py
+++ Usuario_django/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as django_login
 from django.contrib.auth import logout as django_logout
